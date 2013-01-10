7c321f8056c0105b3917efcd32b52c80d3366cc9
Dario Seminara <darios3@gmail.com>
2013-01-10 11:35:02 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tallertest::Application.config.secret_token = '6434c34bb7668c6768ef5e58e66c29913f246ebc31032b0ad82ddcb624efb66ea57e5f4ba8139adcbfeaa64a9864774af96a3d4befee1f71e393c0820fb9fc27'
