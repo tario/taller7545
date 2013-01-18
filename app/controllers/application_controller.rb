15843d07ad51a438e392db247fb6585d906b9047
Dario Seminara <darios3@gmail.com>
2013-01-18 14:56:48 -0300

    separated views for mobile on sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  def mobile_device?
+    request.user_agent =~ /Mobile|webOS/ or params[:mobile] == "1"
+  end
+  helper_method :mobile_device?  
 end
