ac4c90e79f708e69a3c7d7d80137309db93b21ab
Dario Seminara <darios3@gmail.com>
2013-01-22 16:18:25 -0300

    added flash notice for login error

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,12 @@ class SessionsController < ApplicationController
   end
   
   def create
-    session[:current_user_id] = User.authorize(params[:email_username], params[:password]).id
-    redirect_to '/'
+    begin
+      session[:current_user_id] = User.authorize(params[:email_username], params[:password]).id
+      redirect_to '/'
+    rescue SecurityError
+      flash[:error] = "Username/password mismatch"
+      redirect_to :action => :new
+    end
   end
 end
